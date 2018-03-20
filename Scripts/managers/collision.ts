6ae2a14340e09cc25903dc5624f30fb746b9e06d
Abubakir Myrzaly <best.abuka@gmail.com>
2018-03-19 23:29:59 -0400

    Updated baby ts and collision ts, also added vec2 ts and scoreboard ts

--- Scripts/managers/collision.ts
+++ Scripts/managers/collision.ts
@@ -1,22 +1,32 @@
 module managers {
-
-    //Collision Class - the class to provide collisions
     export class Collision {
-        constructor() {
-
-            this.Start();
+  
+  
+      public CheckCollision(object1:objects.GameObject, object2:objects.GameObject):void {
+        // define points for both object1 and object2
+        let P1 = new math.vec2(object1.x, object1.y);
+        let P2 = new Mmath.vec2(object2.x, object2.y);
+  
+        // check if there is a collision
+        if(math.vec2.Distance(P1, P2 ) <  (object1.halfHeight + object2.halfHeight) ) {
+            if(!object2.isColliding) {
+              object2.isColliding = true;
+              switch(object2.name) {
+                case "cupcake":
+                  //createjs.Sound.play("yay");
+                  objects.Game.scoreBoardManager.Lives += 1;
+                break;
+                case "baby":
+                  //createjs.Sound.play("thunder");
+                  objects.Game.scoreBoardManager.Lives -= 1;
+                break;
+              }
+  
+            }
         }
-
-        public Start() {
-
+        else {
+          object2.isColliding = false;
         }
-
-        public Update() {
-
-        }
-
-        /*public CheckCollision(object1: objects.GameObject, object2: objects.GameObject): boolean {
-
-        }*/
+      }
     }
-}
+  }
---
+++ Scripts/math/vec2.js
@@ -0,0 +1,33 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var math;
+(function (math) {
+    var vec2 = /** @class */ (function (_super) {
+        __extends(vec2, _super);
+        //private instance variables
+        //public properties
+        // constructors
+        function vec2(x, y) {
+            if (x === void 0) { x = 0; }
+            if (y === void 0) { y = 0; }
+            return _super.call(this, x, y) || this;
+        }
+        // private methods
+        // public methods
+        // calculates the distance between two Vec2 objects
+        vec2.Distance = function (P1, P2) {
+            return Math.floor(Math.sqrt(Math.pow(P2.x - P1.x, 2) + Math.pow(P2.y - P1.y, 2)));
+        };
+        return vec2;
+    }(createjs.Point));
+    math.vec2 = vec2;
+})(math || (math = {}));
+//# sourceMappingURL=vec2.js.map
